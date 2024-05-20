662413da52f4835d3270bb5ef73b41cd7a6ff209
liamthura <168834227+liamthura@users.noreply.github.com>
2024-05-20 23:44:05 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -195,6 +195,16 @@ def main():
         put_buttons(['Login', 'Toggle Dark Mode'], onclick=[user_login, toggle_dark_mode]).style('float: right;')
 
     put_html(f'<h1>Welcome, {get_username()}</h1>')
+    put_text(f'''
+        This is a simple CRUD application that allows users to log in, register, add, update, and delete heroes.
+The application uses Flask, SQLAlchemy, and PyWebIO to create a simple web application.
+PyWebio is used to create the user interface, while Flask and SQLAlchemy are used to handle the backend logic and database operations.
+I deployed the app to Vercel, and set-up the database (PostgreSQL) on Render.
+The app isn't configured to manage sessions for users. Session management could be added to the app to improve the user experience. Could use Flask's session management capabilities to manage user sessions.
+The app does not make use of Flask's routing capabilities. Routing could be added to the app to create a more structured application.
+
+Made by Khant Thura / Liam
+    ''')
 
     if valid_user is None:
         put_table(get_heroes_table(valid_user), header=['ID', 'Name'])
